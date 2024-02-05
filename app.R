6b015ab0ea273a6b2008f5485a75d13ec0b6b89a
Sally Claridge <sally.e.claridge@gmail.com>
2024-02-05 17:53:42 -0500

    file reorg

--- app.R
+++ app.R
@@ -8,44 +8,64 @@
 #
 
 library(shiny)
+# runExample("01_hello")      # a histogram
+# runExample("02_text")       # tables and data frames
+# runExample("03_reactivity") # a reactive expression
+# runExample("04_mpg")        # global variables
+# runExample("05_sliders")    # slider bars
+# runExample("06_tabsets")    # tabbed panels
+# runExample("07_widgets")    # help text and submit buttons
+# runExample("08_html")       # Shiny app built from HTML
+# runExample("09_upload")     # file upload wizard
+# runExample("10_download")   # file download wizard
+# runExample("11_timer")      # an automated timer
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Functional Genomics Pipeline Processing"),
 
-    # Sidebar with a slider input for number of bins 
+    fluidRow(wellPanel("Top row")),
+
     sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+        # Sidebar Layout Elements (i.e. the panels)
+        sidebarPanel = sidebarPanel(
+            helpText("Helop text!"),
+
+            #Control Widget
+            selectInput(
+                inputId = "input_1",
+                label = "Short description of control widget",
+                choices = c(
+                    "Iowa" = "IA",
+                    "Colorado" = "CO",
+                    "Massachusetts" = "MA")),
+
+            fileInput("file", h3("File input"))
         ),
 
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
+    mainPanel = mainPanel(
+        h1("First level title"),
+        textOutput("selected_var")
         )
     )
 )
 
+    # Grid layout, max 12-wide
+    #
+    # fluidRow(column(width = 4, wellPanel("Bottom row, column 1, width 4")),
+    #          column(width = 8, wellPanel("Bottom row, column 2, width 8")))
+
+
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
+    output$selected_var <- renderText({
+        paste("You have selected this", input$input_1)
     })
+
 }
 
-# Run the application 
+# Run the application
 shinyApp(ui = ui, server = server)
